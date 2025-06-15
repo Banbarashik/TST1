651bdf05e9e31b69efb05903c6603abf0a727c32
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-15 10:18:26 +0400

    draft nav manu

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,64 @@
 "use client";
 
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+
 export default function Test() {
-  return <></>;
+  return (
+    <NavigationMenu.Root className="w-48">
+      <NavigationMenu.List>
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger>Продукция</NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            {/* submenu */}
+            <NavigationMenu.Sub orientation="vertical" className="bg-amber-300">
+              <NavigationMenu.List>
+                <NavigationMenu.Item>
+                  <NavigationMenu.Trigger>
+                    Калориферы водяные
+                  </NavigationMenu.Trigger>
+                  <NavigationMenu.Content>
+                    <div className="flex">
+                      <img src="#" alt="#" className="bg-gray-300" />
+                      <ul>
+                        <li>
+                          <NavigationMenu.Link>КСк</NavigationMenu.Link>
+                        </li>
+                        <li>
+                          <NavigationMenu.Link>ТВВ</NavigationMenu.Link>
+                        </li>
+                        <li>
+                          <NavigationMenu.Link>кфб в</NavigationMenu.Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </NavigationMenu.Content>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item>
+                  <NavigationMenu.Trigger>
+                    Калориферы паровые
+                  </NavigationMenu.Trigger>
+                  <NavigationMenu.Content>
+                    <div className="flex">
+                      <img src="#" alt="#" className="bg-gray-300" />
+                      <ul>
+                        <li>
+                          <NavigationMenu.Link>КПСк</NavigationMenu.Link>
+                        </li>
+                        <li>
+                          <NavigationMenu.Link>КП</NavigationMenu.Link>
+                        </li>
+                        <li>
+                          <NavigationMenu.Link>кфб п</NavigationMenu.Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </NavigationMenu.Content>
+                </NavigationMenu.Item>
+              </NavigationMenu.List>
+            </NavigationMenu.Sub>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
+  );
 }
