99d2c8163448bfdea57d04d46ab43e0c72fe821d
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-17 20:10:56 +0400

    set up basic form functionality

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -14,6 +14,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 // Имя, название организации
 // E-mail
@@ -21,17 +22,27 @@ import {
 // Интересующий продукт
 // Сообщение
 const formSchema = z.object({
-  name: z.string().max(200),
-  email: z.email(),
+  username: z.string().max(200),
+  company: z.string().max(200),
+  email: z.email({
+    error: (iss) =>
+      iss.input === "" || iss.input === undefined
+        ? "Обязательное поле"
+        : "Некорректная электронная почта",
+  }),
   region: z.string().max(200),
   product: z.string().max(300),
-  message: z.string().max(4000),
+  message: z.string().min(1, "Обязательное поле").max(4000),
 });
 
 export default function () {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+    },
   });
 
   // 2. Define a submit handler.
@@ -40,4 +51,38 @@ export default function () {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Ваше имя</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
