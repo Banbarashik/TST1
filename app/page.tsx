5d6c6e2dda5dd45feb7889814b096f72c262afbc
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-20 14:41:07 +0400

    place bg image in contact form section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import Hero from "@/components/hero";
 import ContactForm from "@/components/contactForm";
 import Catalog from "@/components/catalog";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,15 +11,26 @@ export default function Home() {
       <Hero />
       <div className="max-w-8xl px-12 mx-auto">
         <Catalog />
-        <div className="flex justify-center items-center gap-20 mb-32">
-          <div className="text-center">
+      </div>
+
+      {/* CONTACT FORM SECTION START */}
+      <div className="relative w-full flex justify-center items-center gap-20 pb-32 pt-32 overflow-hidden">
+        <Image
+          src="/img/hero/IMG_20190423_090922.jpg"
+          alt="Contact background"
+          fill
+          className="object-cover z-0"
+          priority
+        />
+        <div className="z-10 flex justify-center items-center gap-20 w-full px-12">
+          <div className="text-center max-w-3xl">
             <p className="text-4xl mb-4 font-bold">Есть вопросы?</p>
             <p className="text-xl mb-2">
-              Оставьте заявку через форму справа, и мы свяжемся с вами в самое
-              ближайшее время.
+              Оставьте заявку через форму, и мы свяжемся с вами в ближайшее
+              время.
             </p>
             <p className="text-xl">
-              <span>Или воспользуйтесь нашими </span>
+              <span>Или воспользуйтесь </span>
               <Link href="" className="text-primary">
                 онлайн-инструментами расчета-подбора калориферов.
               </Link>
@@ -27,8 +38,8 @@ export default function Home() {
           </div>
           <ContactForm />
         </div>
-        div
       </div>
+      {/* CONTACT FORM SECTION END */}
     </>
   );
 }
