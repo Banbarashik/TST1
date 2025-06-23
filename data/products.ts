d89d50038df1e5153e248a8174c79463967ec64c
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-24 02:56:28 +0400

    update products data

--- data/products.ts
+++ data/products.ts
@@ -2,7 +2,11 @@ export const products = [
   {
     id: "1",
     name: "Калорифер КСк 2-1",
-    categories: ["kalorifer", "vodyanoy kalorifer"],
+    categories: [
+      "kalorifer",
+      "vodyanoy-kalorifer",
+      "vodayanoy-dvuhriadny-kalorifer",
+    ],
     price: 10,
     img: "",
     description: "Test",
