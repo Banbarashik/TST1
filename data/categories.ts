acf5dcd9af51070bee48a13616d84be342edf678
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-24 02:55:28 +0400

    create data file with categories

---
+++ data/categories.ts
@@ -0,0 +1,20 @@
+export const categoryTree = [
+  {
+    label: "Vodyanoy kalorifer",
+    slug: "vodyanoy-kalorifer",
+    children: [
+      {
+        label: "Kalorifer KSk",
+        slug: "ksk",
+        children: [
+          { label: "Kalorifer KSk 2-1", slug: "ksk-2-1" },
+          { label: "Kalorifer KSk 2-2", slug: "ksk-2-2" },
+        ],
+      },
+      {
+        label: "Kalorifer TVV",
+        slug: "tvv",
+      },
+    ],
+  },
+];
