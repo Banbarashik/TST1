bf7fd729a1a7fdb408bc63286c482c546c785a04
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-24 01:41:15 +0400

    create file with data about products

---
+++ data/products.ts
@@ -0,0 +1,10 @@
+export const products = [
+  {
+    id: "1",
+    name: "Калорифер КСк 2-1",
+    categories: ["karolifer", "vodyanoy kalorifer"],
+    price: 10,
+    img: "",
+    description: "Test",
+  },
+];
