cccafe797d1d8800d2c3f30048772d7bf377393d
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-24 02:06:07 +0400

    fix typo

--- data/products.ts
+++ data/products.ts
@@ -2,7 +2,7 @@ export const products = [
   {
     id: "1",
     name: "Калорифер КСк 2-1",
-    categories: ["karolifer", "vodyanoy kalorifer"],
+    categories: ["kalorifer", "vodyanoy kalorifer"],
     price: 10,
     img: "",
     description: "Test",
