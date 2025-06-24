00d30d520db63723f11f7bb959880809cb4c99bf
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-24 16:57:11 +0400

    display current category name

--- app/catalog/[category]/page.tsx
+++ app/catalog/[category]/page.tsx
@@ -1,4 +1,5 @@
 import { products } from "@/data/products";
+import { categoryTree } from "@/data/categories";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -19,23 +20,32 @@ export default async function Catalog({
 
   console.log(filteredProducts);
 
+  const currentCategoryName = categoryTree.find(
+    (c) => c.slug === category,
+  )?.label;
+
+  console.log(currentCategoryName);
+
   return (
-    <div className="grid grid-cols-3 gap-5">
-      {filteredProducts.map((product) => (
-        <Link href={`/product/${product.id}`} key={product.id}>
-          <div className="rounded-lg border p-4 transition hover:shadow-md">
-            <Image
-              src={product.img}
-              alt={product.name}
-              width={300}
-              height={200}
-              className="mb-2 h-48 w-full object-cover"
-            />
-            <h2 className="text-lg font-semibold">{product.name}</h2>
-            <p className="text-gray-600">${product.price.toFixed(2)}</p>
-          </div>
-        </Link>
-      ))}
+    <div>
+      <h1 className="mb-4 text-2xl font-bold">{currentCategoryName}</h1>
+      <div className="grid grid-cols-3 gap-5">
+        {filteredProducts.map((product) => (
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <div className="rounded-lg border p-4 transition hover:shadow-md">
+              <Image
+                src={product.img}
+                alt={product.name}
+                width={300}
+                height={200}
+                className="mb-2 h-48 w-full object-cover"
+              />
+              <h2 className="text-lg font-semibold">{product.name}</h2>
+              <p className="text-gray-600">${product.price.toFixed(2)}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
