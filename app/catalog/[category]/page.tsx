c5c2abb829c3e01baed33b85c4bc922c18044f79
Banbarashik <odinokyskitalec@yahoo.com>
2025-06-25 12:27:02 +0400

    change product images' dimensions

--- app/catalog/[category]/page.tsx
+++ app/catalog/[category]/page.tsx
@@ -33,8 +33,7 @@ export default async function Catalog({
                 src={product.img}
                 alt={product.name}
                 width={300}
-                height={200}
-                className="mb-2 h-48 w-full object-cover"
+                height={300}
               />
               <h2 className="text-lg font-semibold">{product.name}</h2>
               <p className="text-gray-600">${product.price.toFixed(2)}</p>
