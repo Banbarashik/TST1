58cf637e395a95ed8fa039e83ac0e99b9c37fba4
Banbarashik <odinokyskitalec@yahoo.com>
2025-07-08 10:55:16 +0400

    search for category 1st in 'lib/resolveSlug'

--- lib/resolveSlug.ts
+++ lib/resolveSlug.ts
@@ -4,11 +4,11 @@ import { categoryTree } from "@/data/categories";
 import { findCategoryBySlug } from "@/lib/categoryBySlug";
 
 export function resolveSlug(slug: string) {
-  const product = productData.find((product) => product.id === slug);
-  if (product) return { type: "product", data: product };
-
   const category = findCategoryBySlug(slug, categoryTree);
   if (category) return { type: "category", data: category };
+
+  const product = productData.find((product) => product.id === slug);
+  if (product) return { type: "product", data: product };
 
   return null;
 }
