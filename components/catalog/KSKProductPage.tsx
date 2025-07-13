a88484f814aab9758ac387ff403263ee6a828cd9
Banbarashik <odinokyskitalec@yahoo.com>
2025-07-14 01:04:39 +0400

    KSKProductPage: derive category name to use in TableAndCatalogLinks

--- components/catalog/KSKProductPage.tsx
+++ components/catalog/KSKProductPage.tsx
@@ -11,6 +11,7 @@ import ProductParagraph from "@/components/catalog/productParagraph";
 import SimilarProductLink from "@/components/catalog/similarProductLink";
 import TableAndCatalogLinks from "@/components/catalog/tableAndCatalogLinks";
 
+const categoryNameRegex = /^([А-Яа-яA-Za-z]+)/;
 const sizeRegex = /\d+-(\d+)/;
 const rowsRegex = /-(\d+)$/;
 const rowLabels: Record<number, string> = {
@@ -41,6 +42,7 @@ export default function KSKProductPage({ product }: { product: KSKProduct }) {
 
   const shortNameWithoutHyphen = product.shortName.replace("-", " ");
 
+  const [, categoryName] = product.shortName.match(categoryNameRegex);
   const category = product.categories.includes("ksk")
     ? "ksk"
     : product.categories.includes("kpsk")
@@ -147,7 +149,7 @@ export default function KSKProductPage({ product }: { product: KSKProduct }) {
       <Table tableData={product.tableData[1]} className="mb-10" />
       <TableAndCatalogLinks
         tableURL="#"
-        tableLinkText={`${type?.plu} калориферы КСк`}
+        tableLinkText={`${type?.plu} калориферы ${categoryName}`}
         catalogURL="#"
       />
     </div>
