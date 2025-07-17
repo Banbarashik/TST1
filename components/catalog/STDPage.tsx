2287d1c509ba2afd612000a3f55c4511c15e0465
Banbarashik <odinokyskitalec@yahoo.com>
2025-07-17 16:45:23 +0400

    render elements of calorifier used in std300-ksk

--- components/catalog/STDPage.tsx
+++ components/catalog/STDPage.tsx
@@ -1,7 +1,11 @@
+import { getHeatCarrierAdj } from "@/lib/heatCarrierAdj";
 import ProductCard from "./productCard";
 import ProductSubheader from "./productSubheader";
+import ProductParagraph from "./productParagraph";
 
 export default function STDPage({ product }) {
+  const heatCarrierAdj = getHeatCarrierAdj(product.heatCarrier);
+
   return (
     <div>
       <h1 className="mb-8 text-2xl font-bold uppercase">{product.name}</h1>
@@ -13,6 +17,20 @@ export default function STDPage({ product }) {
             <ProductCard product={{ ...variant, img: product.img, name }} />
             <div>
               <ProductSubheader text={name} />
+              <ProductParagraph>
+                Теплоотдающие элементы {heatCarrierAdj.gen} калорифера{" "}
+                {variant.calorifier}:
+              </ProductParagraph>
+              <ul className="mb-4 text-lg">
+                <li>
+                  - электросварные прямошовные трубки {variant.tubeSize} мм по
+                  ГОСТ 10704-91
+                </li>
+                <li>
+                  - цельнотянутые бесшовные трубки {variant.tubeSize} мм по ГОСТ
+                  8734-75 с алюминиевым (АД1 ТУ 1-8-267-99) накатным оребрением
+                </li>
+              </ul>
             </div>
           </div>
         );
