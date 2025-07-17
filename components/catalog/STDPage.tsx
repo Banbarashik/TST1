ec0085b8a7a6b30ba90dff6b67617a06ccaa9a7c
Banbarashik <odinokyskitalec@yahoo.com>
2025-07-17 16:17:29 +0400

    render variant name

--- components/catalog/STDPage.tsx
+++ components/catalog/STDPage.tsx
@@ -1,19 +1,19 @@
 import ProductCard from "./productCard";
+import ProductSubheader from "./productSubheader";
 
 export default function STDPage({ product }) {
   return (
     <div>
       <h1 className="mb-8 text-2xl font-bold uppercase">{product.name}</h1>
       {product.variants.map(function (variant) {
+        const name = `Воздушно-отопительный агрегат ${variant.model}`;
+
         return (
           <div key={variant.id} className="mb-6 flex items-start gap-4">
-            <ProductCard
-              product={{
-                ...variant,
-                img: product.img,
-                name: `Воздушно-отопительный агрегат ${variant.model}`,
-              }}
-            />
+            <ProductCard product={{ ...variant, img: product.img, name }} />
+            <div>
+              <ProductSubheader text={name} />
+            </div>
           </div>
         );
       })}
