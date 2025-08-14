a88cf6072220484dee14b13edb5e54c06a6ae8aa
Banbarashik <odinokyskitalec@yahoo.com>
2025-08-14 21:40:17 +0400

    add pagination to catalog

--- app/(main)/(catalog)/catalog/[category]/page.tsx
+++ app/(main)/(catalog)/catalog/[category]/page.tsx
@@ -1,9 +1,12 @@
 import { productData } from "@/data/products";
 import { categoryTree } from "@/data/categories";
 
+import Link from "next/link";
+
 import { sortProducts } from "@/lib/utils";
 import { findCategoryBySlug } from "@/lib/categoryBySlug";
 
+import { Button } from "@/components/ui/button";
 import ProductCard from "@/components/catalog/productCard";
 
 const defaultCategory = {
@@ -11,13 +14,16 @@ const defaultCategory = {
   slug: "all",
 };
 
+const PRODUCTS_PER_PAGE = 20;
+
 export default async function Catalog({
   params,
+  searchParams,
 }: {
   params: { category: string };
+  searchParams?: { page?: string };
 }) {
-  const { category: slug } = await params;
-
+  const { category: slug } = params;
   const { title } = findCategoryBySlug(slug, categoryTree) ?? defaultCategory;
 
   const filteredProducts =
@@ -27,6 +33,14 @@ export default async function Catalog({
 
   const sortedProducts = filteredProducts.sort((a, b) =>
     sortProducts(a.name, b.name),
+  );
+
+  const page = Number(searchParams?.page) || 1;
+  const totalPages = Math.ceil(sortedProducts.length / PRODUCTS_PER_PAGE);
+  const startIdx = (page - 1) * PRODUCTS_PER_PAGE;
+  const paginatedProducts = sortedProducts.slice(
+    startIdx,
+    startIdx + PRODUCTS_PER_PAGE,
   );
 
   if (sortedProducts.length === 0)
@@ -40,8 +54,25 @@ export default async function Catalog({
     <div>
       <h1 className="mb-6 text-2xl font-bold uppercase">{title}</h1>
       <div className="mb-20 grid grid-cols-3 gap-5">
-        {sortedProducts.map((product) => (
+        {paginatedProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+      <div className="mb-10 flex justify-center gap-2">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <Button
+            asChild
+            key={i + 1}
+            variant="outline"
+            size="icon"
+            className={
+              page === i + 1
+                ? "bg-primary text-white hover:text-white"
+                : "bg-white"
+            }
+          >
+            <Link href={`?page=${i + 1}`}>{i + 1}</Link>
+          </Button>
         ))}
       </div>
     </div>
--- components/catalog.tsx
+++ components/catalog.tsx
@@ -149,7 +149,6 @@ export default function Catalog() {
                 );
               })}
             </ul>
-            <p>{category.description}</p>
           </li>
         );
       })}
