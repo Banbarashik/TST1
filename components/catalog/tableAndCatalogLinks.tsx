326d6fa984b0b73d30eba9fa4065660b72e69286
Banbarashik <odinokyskitalec@yahoo.com>
2025-08-24 15:31:51 +0400

    modify TableAndCatalogLinks to accept catalogLinkText and props of a tag

--- components/catalog/tableAndCatalogLinks.tsx
+++ components/catalog/tableAndCatalogLinks.tsx
@@ -6,18 +6,25 @@ export default function TableAndCatalogLinks({
   tableURL,
   tableLinkText,
   catalogURL,
+  catalogLinkText = "Скачать каталог PDF",
+  ...props
 }: {
   tableURL: string;
   tableLinkText: string;
   catalogURL: string;
-}) {
+  catalogLinkText?: string;
+} & React.ComponentProps<"a">) {
   return (
     <div className="flex h-12 w-full gap-6">
       <Button className="text-md h-full flex-1/2 bg-gray-300 font-semibold text-black">
-        <Link href={tableURL}>{tableLinkText}</Link>
+        <Link {...props} href={tableURL}>
+          {tableLinkText}
+        </Link>
       </Button>
       <Button className="text-md h-full flex-1/2 bg-gray-300 font-semibold text-black">
-        <Link href={catalogURL}>Скачать каталог PDF</Link>
+        <Link {...props} href={catalogURL}>
+          {catalogLinkText}
+        </Link>
       </Button>
     </div>
   );
