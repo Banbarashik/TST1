eab8c460c400d35ae629e5502f1c8d05928ba9ba
Banbarashik <odinokyskitalec@yahoo.com>
2025-08-28 19:48:50 +0400

    render ksk kpsk table

--- app/kontakty-prajs/page.tsx
+++ app/kontakty-prajs/page.tsx
@@ -10,6 +10,11 @@ export default function ContactsAndPricesPage() {
   const tvv3 = tvv.filter((p) => p.rows === 3);
   const tvv4 = tvv.filter((p) => p.rows === 4);
 
+  const ksk = sortedProducts.filter((p) => p.categories.includes("ksk"));
+  const ksk2 = ksk.filter((p) => p.rows === 2);
+  const ksk3 = ksk.filter((p) => p.rows === 3);
+  const ksk4 = ksk.filter((p) => p.rows === 4);
+
   return (
     <>
       <table>
@@ -26,8 +31,33 @@ export default function ContactsAndPricesPage() {
             <tr>
               <td>ТВВ КП 3{p.size < 10 ? "0" + p.size : p.size}</td>
               <td>{p.price}</td>
-              <td>ТВВ КП 4{p.size}</td>
+              <td>ТВВ КП 4{p.size < 10 ? "0" + p.size : p.size}</td>
               <td>{tvv4[i].price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Наименование калорифера</th>
+            <th>Цена с учетом НДС</th>
+            <th>Наименование калорифера</th>
+            <th>Цена с учетом НДС</th>
+            <th>Наименование калорифера</th>
+            <th>Цена с учетом НДС</th>
+          </tr>
+        </thead>
+        <tbody>
+          {ksk2.map((p, i) => (
+            <tr>
+              <td>КСк КПСк 2-{p.size}</td>
+              <td>{p.price}</td>
+              <td>КСк КПСк 3-{ksk3[i].size}</td>
+              <td>{ksk3[i].price}</td>
+              <td>КСк КПСк 4-{ksk4[i].size}</td>
+              <td>{ksk4[i].price}</td>
             </tr>
           ))}
         </tbody>
