b483ecae74f11237e349b03da8f5b81786676e72
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-04 21:48:09 +0400

    change menu items in nav menu

--- components/navigationMenu.tsx
+++ components/navigationMenu.tsx
@@ -5,29 +5,74 @@ import Link from "next/link";
 export default function NavigationMenu() {
   return (
     <div className="flex h-full items-center">
-      <Link
-        href="/kalorifery-voda"
-        className="hover:bg-accent flex h-full flex-col items-center justify-center px-4 font-bold"
-      >
-        <span>Калькулятор подбора</span>
-        <span>водяных калориферов</span>
-      </Link>
-      <span className="h-full w-px bg-gray-400"></span>
-      <Link
-        href="/kalorifery-par"
-        className="hover:bg-accent flex h-full flex-col items-center justify-center px-4 font-bold"
-      >
-        <span>Калькулятор подбора</span>
-        <span>паровых калориферов</span>
-      </Link>
-      <span className="h-full w-px bg-gray-400"></span>
-      <Link
-        href="/kontakty-prajs"
-        className="hover:bg-accent flex h-full flex-col items-center justify-center px-4 font-bold"
-      >
-        <span>Контакты</span>
-        <span>Прайс-лист</span>
-      </Link>
+      <div className="h-[82px]">
+        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+      </div>
+      <div className="btn btn-three w-50">
+        <div className="flex flex-col">
+          <span className="h-px bg-[#BDBDBD]"></span>
+          <span className="h-px bg-[#F2F2F2]"></span>
+        </div>
+        <div className="flex flex-col py-4">
+          <span>Калькулятор подбора</span>
+          <span>водяных калориферов</span>
+        </div>
+        <div className="flex flex-col">
+          <span className="h-px bg-[#F2F2F2]"></span>
+          <span className="h-px bg-[#BDBDBD]"></span>
+        </div>
+      </div>
+      {/*      <div className="h-[82px]">
+        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+      </div> */}
+
+      <div className="h-[82px]">
+        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+      </div>
+      <div className="btn btn-three w-51">
+        <div className="flex flex-col">
+          <span className="h-px bg-[#BDBDBD]"></span>
+          <span className="h-px bg-[#F2F2F2]"></span>
+        </div>
+        <div className="flex flex-col py-4">
+          <span>Калькулятор подбора</span>
+          <span>паровых калориферов</span>
+        </div>
+        <div className="flex flex-col">
+          <span className="h-px bg-[#F2F2F2]"></span>
+          <span className="h-px bg-[#BDBDBD]"></span>
+        </div>
+      </div>
+      {/*  <div className="h-[82px]">
+        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+      </div> */}
+
+      <div className="h-[82px]">
+        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+      </div>
+      <div className="btn btn-three w-30">
+        <div className="flex flex-col">
+          <span className="h-px bg-[#BDBDBD]"></span>
+          <span className="h-px bg-[#F2F2F2]"></span>
+        </div>
+        <div className="flex flex-col py-4">
+          <span>Контакты</span>
+          <span>Прайс-лист</span>
+        </div>
+        <div className="flex flex-col">
+          <span className="h-px bg-[#F2F2F2]"></span>
+          <span className="h-px bg-[#BDBDBD]"></span>
+        </div>
+      </div>
+      <div className="h-[82px]">
+        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+      </div>
     </div>
   );
 }
