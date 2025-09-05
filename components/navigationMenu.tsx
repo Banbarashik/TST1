46bee03cf1d3d221fe671a227c9e90532b2ba01e
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-05 21:39:53 +0400

    nav menu btns change

--- components/navigationMenu.tsx
+++ components/navigationMenu.tsx
@@ -4,26 +4,11 @@ import Link from "next/link";
 
 export default function NavigationMenu() {
   return (
-    <div className="text-primary-darker flex h-full items-center">
+    <div className="text-primary-darker flex h-full items-center gap-1.5 pb-1.5 text-[15px]">
       <Link
         href="/kalorifery-voda"
-        className="btn relative flex h-full items-center px-5"
+        className="btn-3 flex h-[93%] items-center px-5"
       >
-        {/* left */}
-        <div className="absolute left-0 h-full">
-          <span className="inline-block h-full w-[2px] bg-[#BDBDBD]"></span>
-          <span className="inline-block h-full w-[2px] bg-[#F2F2F2]"></span>
-        </div>
-        {/* top */}
-        <div className="absolute top-0 left-0 flex w-full flex-col">
-          <span className="h-px bg-[#BDBDBD]"></span>
-          <span className="h-px bg-[#F2F2F2]"></span>
-        </div>
-        {/* bottom */}
-        <div className="absolute bottom-0 left-0 flex w-full flex-col">
-          <span className="h-px bg-[#F2F2F2]"></span>
-          <span className="h-px bg-[#BDBDBD]"></span>
-        </div>
         <div className="flex flex-col items-center">
           <span>Калькулятор подбора</span>
           <span>водяных калориферов</span>
@@ -32,23 +17,8 @@ export default function NavigationMenu() {
 
       <Link
         href="/kalorifery-voda"
-        className="btn relative flex h-full items-center px-5"
+        className="btn-3 flex h-[93%] items-center px-5"
       >
-        {/* left */}
-        <div className="absolute left-0 h-full">
-          <span className="inline-block h-full w-[2px] bg-[#BDBDBD]"></span>
-          <span className="inline-block h-full w-[2px] bg-[#F2F2F2]"></span>
-        </div>
-        {/* top */}
-        <div className="absolute top-0 left-0 flex w-full flex-col">
-          <span className="h-px bg-[#BDBDBD]"></span>
-          <span className="h-px bg-[#F2F2F2]"></span>
-        </div>
-        {/* bottom */}
-        <div className="absolute bottom-0 left-0 flex w-full flex-col">
-          <span className="h-px bg-[#F2F2F2]"></span>
-          <span className="h-px bg-[#BDBDBD]"></span>
-        </div>
         <div className="flex flex-col items-center">
           <span>Калькулятор подбора</span>
           <span>паровых калориферов</span>
@@ -57,28 +27,8 @@ export default function NavigationMenu() {
 
       <Link
         href="/kalorifery-voda"
-        className="btn relative flex h-full items-center px-5"
+        className="btn-3 flex h-[93%] items-center px-5"
       >
-        {/* left */}
-        <div className="absolute left-0 h-full">
-          <span className="inline-block h-full w-[2px] bg-[#BDBDBD]"></span>
-          <span className="inline-block h-full w-[2px] bg-[#F2F2F2]"></span>
-        </div>
-        {/* top */}
-        <div className="absolute top-0 left-0 flex w-full flex-col">
-          <span className="h-px bg-[#BDBDBD]"></span>
-          <span className="h-px bg-[#F2F2F2]"></span>
-        </div>
-        {/* bottom */}
-        <div className="absolute bottom-0 left-0 flex w-full flex-col">
-          <span className="h-px bg-[#F2F2F2]"></span>
-          <span className="h-px bg-[#BDBDBD]"></span>
-        </div>
-        {/* right */}
-        <div className="absolute right-0 h-full">
-          <span className="inline-block h-full w-[2px] bg-[#F2F2F2]"></span>
-          <span className="inline-block h-full w-[2px] bg-[#BDBDBD]"></span>
-        </div>
         <div className="flex flex-col items-center">
           <span>Контакты</span>
           <span>Прайс-лист</span>
