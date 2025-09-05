99a632bf3540f5c949bdc4eaaedf426076203334
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-05 16:06:58 +0400

    make nav btns fore rigid

--- components/navigationMenu.tsx
+++ components/navigationMenu.tsx
@@ -5,74 +5,75 @@ import Link from "next/link";
 export default function NavigationMenu() {
   return (
     <div className="text-primary-darker flex h-full items-center">
-      <div className="h-[82px]">
-        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
-        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
-      </div>
-      <Link href="/kalorifery-voda" className="btn btn-three w-50">
-        <div className="flex flex-col">
+      <Link
+        href="/kalorifery-voda"
+        className="btn relative flex h-full items-center px-5"
+      >
+        <div className="absolute left-0 h-full">
+          <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+          <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        </div>
+        <div className="absolute top-0 left-0 flex w-full flex-col">
           <span className="h-px bg-[#BDBDBD]"></span>
           <span className="h-px bg-[#F2F2F2]"></span>
         </div>
-        <div className="flex flex-col py-4">
+        <div className="flex flex-col items-center">
           <span>Калькулятор подбора</span>
           <span>водяных калориферов</span>
         </div>
-        <div className="flex flex-col">
+        <div className="absolute bottom-0 left-0 flex w-full flex-col">
           <span className="h-px bg-[#F2F2F2]"></span>
           <span className="h-px bg-[#BDBDBD]"></span>
         </div>
       </Link>
-      {/*      <div className="h-[82px]">
-        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
-        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
-      </div> */}
 
-      <div className="h-[82px]">
-        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
-        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
-      </div>
-      <Link href="/kalorifery-par" className="btn btn-three w-51">
-        <div className="flex flex-col">
+      <Link
+        href="/kalorifery-voda"
+        className="btn relative flex h-full items-center px-5"
+      >
+        <div className="absolute left-0 h-full">
+          <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+          <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        </div>
+        <div className="absolute top-0 left-0 flex w-full flex-col">
           <span className="h-px bg-[#BDBDBD]"></span>
           <span className="h-px bg-[#F2F2F2]"></span>
         </div>
-        <div className="flex flex-col py-4">
+        <div className="flex flex-col items-center">
           <span>Калькулятор подбора</span>
           <span>паровых калориферов</span>
         </div>
-        <div className="flex flex-col">
+        <div className="absolute bottom-0 left-0 flex w-full flex-col">
           <span className="h-px bg-[#F2F2F2]"></span>
           <span className="h-px bg-[#BDBDBD]"></span>
         </div>
       </Link>
-      {/*  <div className="h-[82px]">
-        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
-        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
-      </div> */}
 
-      <div className="h-[82px]">
-        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
-        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
-      </div>
-      <Link href="/kontakty-prajs" className="btn btn-three w-30">
-        <div className="flex flex-col">
+      <Link
+        href="/kalorifery-voda"
+        className="btn relative flex h-full items-center px-5"
+      >
+        <div className="absolute left-0 h-full">
+          <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+          <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        </div>
+        <div className="absolute top-0 left-0 flex w-full flex-col">
           <span className="h-px bg-[#BDBDBD]"></span>
           <span className="h-px bg-[#F2F2F2]"></span>
         </div>
-        <div className="flex flex-col py-4">
+        <div className="flex flex-col items-center">
           <span>Контакты</span>
           <span>Прайс-лист</span>
         </div>
-        <div className="flex flex-col">
+        <div className="absolute bottom-0 left-0 flex w-full flex-col">
           <span className="h-px bg-[#F2F2F2]"></span>
           <span className="h-px bg-[#BDBDBD]"></span>
         </div>
+        <div className="absolute right-0 h-full">
+          <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
+          <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
+        </div>
       </Link>
-      <div className="h-[82px]">
-        <span className="inline-block h-full w-px bg-[#F2F2F2]"></span>
-        <span className="inline-block h-full w-px bg-[#BDBDBD]"></span>
-      </div>
     </div>
   );
 }
