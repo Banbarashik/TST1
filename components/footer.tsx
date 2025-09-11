7ec4fb4a48e5e15ef3a4efd8ebe9cb5490aacbb7
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-11 21:13:28 +0400

    footer: style, layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,47 +9,46 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="bg-secondary text-secondary-foreground flex h-42 items-center justify-end gap-26 px-20"
+      className="bg-secondary text-secondary-foreground flex h-42 items-center justify-between pr-30 pl-20"
     >
-      <div className="flex items-center gap-2">
-        Создание сайта:
-        <ul className="text-accent space-y-1">
+      <div className="space-y-1 text-[15px]">
+        <p className="mb-2">Разработка сайта:</p>
+        <Link
+          href="https://github.com/Banbarashik"
+          target="_blank"
+          className="text-accent flex items-center gap-2"
+        >
+          <GithubIcon />
+          Banbarashik
+        </Link>
+        <a href="" className="text-accent flex items-center gap-2">
+          <Mail size={20} />
+          odinokiyskitalec@gmail.com
+        </a>
+      </div>
+      <div className="flex gap-20">
+        <ul className="font-medium">
           <li>
-            <Link
-              href="https://github.com/Banbarashik"
-              target="_blank"
-              className="flex items-center gap-2"
-            >
-              <GithubIcon />
-              Banbarashik
-            </Link>
+            Юр. адрес: г. Новосибирск, ул. Широкая, здание 1 А, офис 207/1
+          </li>
+          <li>Почтовый адрес: 652710, г. Киселевск, ул. Юргинская, 1</li>
+        </ul>
+        <ul>
+          <li>
+            <span className="font-semibold">8-961-737-83-14</span> - технические
+            вопросы
           </li>
           <li>
-            <a href="" className="flex items-center gap-2">
-              <Mail size={20} />
-              odinokiyskitalec@gmail.com
-            </a>
+            <span className="font-semibold">8-904-968-14-88</span> - отдел
+            продаж
           </li>
         </ul>
-      </div>
-      <ul className="font-medium">
-        <li>Юр. адрес: г. Новосибирск, ул. Широкая, здание 1 А, офис 207/1</li>
-        <li>Почтовый адрес: 652710, г. Киселевск, ул. Юргинская, 1</li>
-      </ul>
-      <ul>
-        <li>
-          <span className="font-semibold">8-961-737-83-14</span> - технические
-          вопросы
-        </li>
-        <li>
-          <span className="font-semibold">8-904-968-14-88</span> - отдел продаж
-        </li>
-      </ul>
-      <div className="flex gap-6 text-xl">
-        <Logo place="footer" />
-        <div>
-          <p className="text-accent">+7 (3846) 68-23-24</p>
-          <p className="text-accent">zao_tst@mail.ru</p>
+        <div className="flex gap-6 text-lg">
+          <Logo place="footer" />
+          <div>
+            <p className="text-accent">+7 (3846) 68-23-24</p>
+            <p className="text-accent">zao_tst@mail.ru</p>
+          </div>
         </div>
       </div>
     </footer>
