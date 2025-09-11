ec42b6a9cf08dbb73384778302d774469d53bfac
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-11 20:19:20 +0400

    footer: change order of items

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,28 +4,26 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="bg-secondary text-secondary-foreground flex h-42 items-center"
+      className="bg-secondary text-secondary-foreground flex h-42 items-center justify-end gap-26 px-26"
     >
-      <div className="flex w-full justify-center gap-16">
-        <ul className="text-lg font-bold">
-          <li className="text-accent mb-2">+7 (3846) 68-23-24</li>
-          <li>
-            8-961-737-83-14{" "}
-            <span className="font-medium"> - технические вопросы</span>
-          </li>
-          <li>
-            8-904-968-14-88 <span className="font-medium">- отдел продаж</span>
-          </li>
-        </ul>
-        <ul className="font-medium">
-          <li>
-            Юр. адрес: г. Новосибирск, ул. Широкая, здание 1 А, офис 207/1
-          </li>
-          <li>Почтовый адрес: 652710, г. Киселевск, ул. Юргинская, 1</li>
-        </ul>
-        <div className="flex flex-col items-end gap-3">
-          <p className="text-accent text-xl font-medium">zao_tst@mail.ru</p>
-          <Logo place="footer" />
+      <ul className="font-medium">
+        <li>Юр. адрес: г. Новосибирск, ул. Широкая, здание 1 А, офис 207/1</li>
+        <li>Почтовый адрес: 652710, г. Киселевск, ул. Юргинская, 1</li>
+      </ul>
+      <ul>
+        <li>
+          <span className="font-semibold">8-961-737-83-14</span> - технические
+          вопросы
+        </li>
+        <li>
+          <span className="font-semibold">8-904-968-14-88</span> - отдел продаж
+        </li>
+      </ul>
+      <div className="flex gap-6 text-xl">
+        <Logo place="footer" />
+        <div>
+          <p className="text-accent">+7 (3846) 68-23-24</p>
+          <p className="text-accent">zao_tst@mail.ru</p>
         </div>
       </div>
     </footer>
