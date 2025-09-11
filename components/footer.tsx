2a128ccbd7e4b45f2eaa7186f1d7e5aea8984418
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-11 20:41:45 +0400

    footer: add new items

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,37 @@
+import Link from "next/link";
+
+import { Mail } from "lucide-react";
+import { GithubIcon } from "@/components/icons/github";
+
 import Logo from "@/components/ui/logo";
 
 export default function Footer() {
   return (
     <footer
       id="footer"
-      className="bg-secondary text-secondary-foreground flex h-42 items-center justify-end gap-26 px-26"
+      className="bg-secondary text-secondary-foreground flex h-42 items-center justify-end gap-26 px-20"
     >
+      <div className="flex items-center gap-2">
+        Создание сайта:
+        <ul className="text-accent space-y-1">
+          <li>
+            <Link
+              href="https://github.com/Banbarashik"
+              target="_blank"
+              className="flex items-center gap-2"
+            >
+              <GithubIcon />
+              Banbarashik
+            </Link>
+          </li>
+          <li>
+            <a href="" className="flex items-center gap-2">
+              <Mail size={20} />
+              odinokiyskitalec@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
       <ul className="font-medium">
         <li>Юр. адрес: г. Новосибирск, ул. Широкая, здание 1 А, офис 207/1</li>
         <li>Почтовый адрес: 652710, г. Киселевск, ул. Юргинская, 1</li>
