3bb5bece5ba0b24eb49e4698b6489e011aa122e4
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-12 11:06:25 +0400

    set up sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+import { categoryTree } from "@/data/categories";
+import { productData } from "@/data/products";
+
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [{}];
+}
