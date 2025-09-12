244dcc185d6462dd760401c970d5621dc487442d
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-12 11:33:33 +0400

    sitemap: enter pages manually

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,5 +4,120 @@ import { productData } from "@/data/products";
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [{}];
+  return [
+    {
+      url: "https://www.zao-tst.ru",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://www.zao-tst.ru/kontakty-prajs",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://www.zao-tst.ru/personal-data",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.0,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-voda",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-par",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-ksk",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-kpsk",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-tvv",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-kp",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-kfb-a",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.zao-tst.ru/kalorifery-kfb",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.zao-tst.ru/ao2-ksk-kpsk",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/ao2-kpsk-ksk",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/avo-tvv-kp",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.zao-tst.ru/std300-ksk-kpsk",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.zao-tst.ru/std300-tvv-kp",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.zao-tst.ru/elektronagrevateli",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.zao-tst.ru/teploventilyatory",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
+      url: "https://www.zao-tst.ru/shkafy-upravleniya",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+  ];
 }
