9ab2ed96bad4cb94da60cceac761133ae07c8725
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-13 16:44:09 +0400

    nav_btns: complete

--- components/navigationMenu.tsx
+++ components/navigationMenu.tsx
@@ -4,50 +4,26 @@ import Link from "next/link";
 
 export default function NavigationMenu() {
   return (
-    <div className="flex items-center">
-      <a
-        href="#"
-        className="btn-flip w-min"
-        data-back="Паровые калориферы"
-        data-front="Калькулятор подбора"
-      ></a>
-      <a
-        href="#"
-        className="btn-flip w-min"
-        data-back="Паровые калориферы"
-        data-front="Калькулятор подбора"
-      ></a>
-    </div>
-    /*     <div className="text-primary-darker 3xl:text-[15px] flex h-full items-center gap-1.5 pb-1.5 lg:text-[13px]">
+    <div className="flex">
       <Link
         href="/kalorifery-voda"
-        className="btn-3 flex h-[93%] items-center px-5"
-      >
-        <div className="flex flex-col items-center">
-          <span>Калькулятор подбора</span>
-          <span>водяных калориферов</span>
-        </div>
-      </Link>
-
+        className="btn-flip w-min"
+        data-back="Водяные калориферы"
+        data-front="Калькулятор подбора"
+      />
       <Link
         href="/kalorifery-par"
-        className="btn-3 flex h-[93%] items-center px-5"
-      >
-        <div className="flex flex-col items-center">
-          <span>Калькулятор подбора</span>
-          <span>паровых калориферов</span>
-        </div>
-      </Link>
-
+        className="btn-flip w-min"
+        data-back="Паровые калориферы"
+        data-front="Калькулятор подбора"
+      />
       <Link
         href="/kontakty-prajs"
-        className="btn-3 flex h-[93%] items-center px-5"
-      >
-        <div className="flex flex-col items-center">
-          <span>Контакты</span>
-          <span>Прайс-лист</span>
-        </div>
-      </Link>
-    </div> */
+        id="btn0"
+        className="btn-flip w-max"
+        data-back="Контакты"
+        data-front="Прайс"
+      />
+    </div>
   );
 }
