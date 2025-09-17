df3a2535d18ea1272ac69799f774e379a6bfa567
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-17 13:10:02 +0400

    next.config.ts: add redirects for old pages which aren't built yet

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,66 @@ const nextConfig: NextConfig = {
         destination: "/:slug",
         permanent: true,
       },
+      {
+        source: "/raschet-online-vodianyh-kaloriferov",
+        destination: "/kalorifery-voda",
+        permanent: true,
+      },
+      {
+        source: "/raschet-online-parovyh-kaloriferov",
+        destination: "/kalorifery-par",
+        permanent: true,
+      },
+      {
+        source: "/raschet-online-elektricheskih-kaloriferov",
+        destination: "/elektronagrevateli",
+        permanent: true,
+      },
+      {
+        source: "/raschet-podbor-kaloriferov",
+        destination: "/kalorifery-voda",
+        permanent: true,
+      },
+      {
+        source: "/koefficient-teploperedachi-kaloriferov",
+        destination: "/kalorifery-voda",
+        permanent: true,
+      },
+      {
+        source: "/gidravlicheskoe-soprotivlenie-kaloriferov",
+        destination: "/kalorifery-voda",
+        permanent: true,
+      },
+      {
+        source: "/podbor-raschet-kaloriferov",
+        destination: "/kalorifery-par",
+        permanent: true,
+      },
+      {
+        source: "/koefficient-teploperedachi-parovyh-kaloriferov",
+        destination: "/kalorifery-par",
+        permanent: true,
+      },
+      {
+        source: "/komplektatciia-kaloriferov",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/zamena-kaloriferov",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/kalorifery-kfb-kfs",
+        destination: "/kalorifery-kfb",
+        permanent: true,
+      },
+      {
+        source: "/kalorifery-kms-kmb",
+        destination: "/kalorifery-kfb-a",
+        permanent: true,
+      },
     ];
   },
 };
