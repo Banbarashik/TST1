16435c454c6ce7da9de69bddfb2f58e983362e32
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-17 17:15:17 +0400

    nav_menu: change text size

--- components/navigationMenu.tsx
+++ components/navigationMenu.tsx
@@ -7,25 +7,25 @@ export default function NavigationMenu() {
     <div className="flex">
       <Link
         href="/kalorifery-voda"
-        className="btn-flip btn-flip-p w-min"
+        className="btn-flip btn-flip-p w-min text-sm xl:text-base"
         data-back="Сертификаты"
         data-front="Продукция"
       />
       <Link
         href="/kontakty-prajs"
-        className="btn-flip btn-flip-p w-min"
+        className="btn-flip btn-flip-p w-min text-sm xl:text-base"
         data-back="Прайс лист"
         data-front="Контакты"
       />
       <Link
         href="/kalorifery-voda"
-        className="btn-flip w-min"
+        className="btn-flip w-min text-sm xl:text-base"
         data-back="Водяные калориферы"
         data-front="Калькулятор подбора"
       />
       <Link
         href="/kalorifery-par"
-        className="btn-flip w-min"
+        className="btn-flip w-min text-sm xl:text-base"
         data-back="Паровые калориферы"
         data-front="Калькулятор подбора"
       />
