62c84cbeb0e965d752dcc93d3f31ad113125787c
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-18 20:04:06 +0400

    footer: adapt for 768-1023

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,56 +9,59 @@ export default function Footer() {
   return (
     <footer
       id="footer"
-      className="bg-secondary text-secondary-foreground relative py-4 pr-4 sm:pb-10 sm:pl-26"
+      className="bg-secondary text-secondary-foreground relative py-4 pr-4 sm:pb-10 sm:pl-26 lg:flex lg:h-60 lg:justify-end lg:gap-16"
     >
-      <div className="mb-6 flex items-center justify-center gap-4 sm:justify-start">
-        <Logo place="footer" />
-        <div className="text-lg">
-          <p className="text-accent">+7 (3846) 68-23-24</p>
-          <a href="mailto:zao_tst@mail.ru" className="text-accent">
-            zao_tst@mail.ru
-          </a>
+      <div>
+        <div className="mb-6 flex items-center justify-center gap-4 sm:justify-start lg:order-last lg:m-0">
+          <Logo place="footer" />
+          <div className="text-lg">
+            <p className="text-accent">+7 (3846) 68-23-24</p>
+            <a href="mailto:zao_tst@mail.ru" className="text-accent">
+              zao_tst@mail.ru
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="space-y-3 text-end sm:text-start">
-        <ul>
-          <li className="flex flex-col sm:flex-row sm:gap-2">
-            <div>
-              Технические вопросы<span className="hidden sm:inline">:</span>
-            </div>
-            <div className="font-semibold">8-961-737-83-14</div>
-          </li>
-          <li className="flex flex-col sm:flex-row sm:gap-2">
-            <div>
-              Отдел продаж<span className="hidden sm:inline">:</span>
-            </div>
-            <div className="font-semibold">8-904-968-14-88</div>
-          </li>
-        </ul>
-        <ul className="space-y-2 font-medium">
-          <li className="flex flex-col sm:flex-row sm:flex-wrap sm:gap-x-2">
-            <div>
-              Юридический адрес<span className="hidden sm:inline">:</span>
-            </div>
-            {/* 0-639 */}
-            <div className="sm:hidden">г. Новосибирск, ул. Широкая,</div>
-            <div className="sm:hidden">здание 1 А, офис 207/1</div>
-            {/* 640-767 */}
-            <div className="hidden sm:block">
-              г. Новосибирск, ул. Широкая, здание 1 А,
-            </div>
-            <div className="hidden sm:block">офис 207/1</div>
-          </li>
-          <li className="flex flex-col sm:flex-row sm:gap-2">
-            <div>
-              Почтовый адрес<span className="hidden sm:inline">:</span>
-            </div>
-            <div>
-              г. Киселевск, ул. Юргинская<span className="sm:hidden">, 1</span>
-              <span className="hidden sm:inline">, дом 1</span>
-            </div>
-          </li>
-        </ul>
+        <div className="space-y-3 text-end sm:text-start">
+          <ul>
+            <li className="flex flex-col sm:flex-row sm:gap-2">
+              <div>
+                Технические вопросы<span className="hidden sm:inline">:</span>
+              </div>
+              <div className="font-semibold">8-961-737-83-14</div>
+            </li>
+            <li className="flex flex-col sm:flex-row sm:gap-2">
+              <div>
+                Отдел продаж<span className="hidden sm:inline">:</span>
+              </div>
+              <div className="font-semibold">8-904-968-14-88</div>
+            </li>
+          </ul>
+          <ul className="space-y-2 font-medium">
+            <li className="flex flex-col sm:flex-row sm:flex-wrap sm:gap-x-2">
+              <div>
+                Юридический адрес<span className="hidden sm:inline">:</span>
+              </div>
+              {/* 0-639 */}
+              <div className="sm:hidden">г. Новосибирск, ул. Широкая,</div>
+              <div className="sm:hidden">здание 1 А, офис 207/1</div>
+              {/* 640-767 */}
+              <div className="hidden sm:block">
+                г. Новосибирск, ул. Широкая, здание 1 А,
+              </div>
+              <div className="hidden sm:block">офис 207/1</div>
+            </li>
+            <li className="flex flex-col sm:flex-row sm:gap-2">
+              <div>
+                Почтовый адрес<span className="hidden sm:inline">:</span>
+              </div>
+              <div>
+                г. Киселевск, ул. Юргинская
+                <span className="sm:hidden">, 1</span>
+                <span className="hidden sm:inline">, дом 1</span>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="absolute bottom-25 -left-15 -rotate-90 space-y-1 text-[15px]">
         <p>Разработка сайта:</p>
