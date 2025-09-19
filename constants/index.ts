f73184dc4319c90c35a8c4688479b0ff7b4a5198
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-19 12:58:44 +0400

    contants: add SITE_URL

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,4 @@
 export const FORM_STORAGE_KEY = "contactFormData";
 export const CONTACT_FORM_SEND_TO_EMAIL = "zao_tst@mail.ru";
 export const YM_COUNTER_ID = "19034332";
+export const SITE_URL = "https://zao-tst.ru";
