05399d7018897386d4e52ae64825d906238a430c
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-21 16:10:24 +0400

    SupplyCalorifierPage: make the table scroll

--- components/catalog/supplyCalorifierPage.tsx
+++ components/catalog/supplyCalorifierPage.tsx
@@ -113,82 +113,83 @@ export default function SupplyCalorifierPage({
         )}
       </ProductParagraph>
       <ProductSubheader text={`Технические характеристики ${shortName}`} />
-      <table className="single-table water-and-steam water-and-steam-inner mb-1 w-full">
-        <thead>
-          <tr>
-            <th rowSpan={2}>
-              Производительность <br /> по воздуху, м<sup>3</sup>/час
-            </th>
-            <th colSpan={5}>
-              Габаритные и <br /> присоединительные размеры, мм
-            </th>
-            <th colSpan={isWater ? 2 : 1} className="dy">
-              dy
-            </th>
-            <th colSpan={3}>
-              Площадь поверхности <br /> теплообмена, м<sup>2</sup>
-            </th>
-            <th colSpan={3} className="mass">
-              Масса, кг
-            </th>
-          </tr>
-          <tr>
-            <th className="small-cols">
-              {isWater && "L"}
-              {isSteam && "H"}
-              <br />
-              {isWater && "H"}
-              {isSteam && "B"}
-            </th>
-            <th className="small-cols">
-              {isWater && "L1"}
-              {isSteam && "H1"}
-              <br />
-              {isWater && "H1"}
-              {isSteam && "B1"}
-            </th>
-            <th className="small-cols">
-              {isWater && "L2"}
-              {isSteam && "H2"}
-              <br />
-              {isWater && "H2"}
-              {isSteam && "B2"}
-            </th>
-            <th className="small-cols">
-              {isWater && "L3"}
-              {isSteam && "H3"}
-            </th>
-            <th className="small-cols">C</th>
-            <th className="small-cols w-10">мм</th>
-            {isWater && <th className="small-cols w-10 pt-1">&quot;</th>}
-            <th className="kal2">{nameAbbrev}2</th>
-            <th className="kal2">{nameAbbrev}3</th>
-            <th className="kal2">{nameAbbrev}4</th>
-            <th className="kal2">{nameAbbrev}2</th>
-            <th className="kal2">{nameAbbrev}3</th>
-            <th className="kal2">{nameAbbrev}4</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {product.specsTableValues.map((value, i) => {
-              const fractionMatch = String(value).match(
-                /^(\d+)\s+(\d+)\/(\d+)$/,
-              );
-              if (fractionMatch) {
-                const [, whole, numerator, denominator] = fractionMatch;
-                return (
-                  <td key={i}>
-                    {whole} <sup>{numerator}</sup>/<sub>{denominator}</sub>
-                  </td>
+      <div className="w-full overflow-x-auto">
+        <table className="single-table water-and-steam water-and-steam-inner mb-1 w-full min-w-231">
+          <thead>
+            <tr>
+              <th rowSpan={2}>
+                Производительность <br /> по воздуху, м<sup>3</sup>/час
+              </th>
+              <th colSpan={5}>
+                Габаритные и <br /> присоединительные размеры, мм
+              </th>
+              <th colSpan={isWater ? 2 : 1} className="dy">
+                dy
+              </th>
+              <th colSpan={3}>
+                Площадь поверхности <br /> теплообмена, м<sup>2</sup>
+              </th>
+              <th colSpan={3} className="mass">
+                Масса, кг
+              </th>
+            </tr>
+            <tr>
+              <th className="small-cols">
+                {isWater && "L"}
+                {isSteam && "H"}
+                <br />
+                {isWater && "H"}
+                {isSteam && "B"}
+              </th>
+              <th className="small-cols">
+                {isWater && "L1"}
+                {isSteam && "H1"}
+                <br />
+                {isWater && "H1"}
+                {isSteam && "B1"}
+              </th>
+              <th className="small-cols">
+                {isWater && "L2"}
+                {isSteam && "H2"}
+                <br />
+                {isWater && "H2"}
+                {isSteam && "B2"}
+              </th>
+              <th className="small-cols">
+                {isWater && "L3"}
+                {isSteam && "H3"}
+              </th>
+              <th className="small-cols">C</th>
+              <th className="small-cols w-10">мм</th>
+              {isWater && <th className="small-cols w-10 pt-1">&quot;</th>}
+              <th className="kal2">{nameAbbrev}2</th>
+              <th className="kal2">{nameAbbrev}3</th>
+              <th className="kal2">{nameAbbrev}4</th>
+              <th className="kal2">{nameAbbrev}2</th>
+              <th className="kal2">{nameAbbrev}3</th>
+              <th className="kal2">{nameAbbrev}4</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {product.specsTableValues.map((value, i) => {
+                const fractionMatch = String(value).match(
+                  /^(\d+)\s+(\d+)\/(\d+)$/,
                 );
-              }
-
-              return <td key={i}>{value}</td>;
-            })}
-          </tr>
-        </tbody>
-      </table>
+                if (fractionMatch) {
+                  const [, whole, numerator, denominator] = fractionMatch;
+                  return (
+                    <td key={i}>
+                      {whole} <sup>{numerator}</sup>/<sub>{denominator}</sub>
+                    </td>
+                  );
+                }
+                return <td key={i}>{value}</td>;
+              })}
+            </tr>
+          </tbody>
+        </table>
+      </div>
       {drawing && (
         <Image
           src={drawing}
