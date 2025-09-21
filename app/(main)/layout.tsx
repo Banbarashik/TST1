60d91134d7ead3e45afa3f87c384eb6f2e3a890f
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-21 12:31:16 +0400

    catalog layout: isMobile - till 767

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -9,7 +9,7 @@ export default function CatalogLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isMobile = useMediaQuery("(max-width: 640px)");
+  const isMobile = useMediaQuery("(max-width: 767px)");
 
   return (
     <main className="3xl:pb-14 mx-auto flex w-full max-w-[1300px] flex-1 flex-col gap-10 px-3 pt-6 lg:pt-14 lg:pb-20">
