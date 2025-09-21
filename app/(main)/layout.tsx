bb3048bb87bc1122f868edafac777a96becce7b1
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-21 15:02:31 +0400

    catalog layout: remove px on >=1400

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -4,6 +4,7 @@ import Sidebar from "@/components/catalog/sidebar";
 
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 
+// TODO rename cause not only cataog, but all (main) pages
 export default function CatalogLayout({
   children,
 }: Readonly<{
@@ -12,7 +13,7 @@ export default function CatalogLayout({
   const isMobile = useMediaQuery("(max-width: 1023px)");
 
   return (
-    <main className="3xl:pb-14 mx-auto flex w-full max-w-[1300px] flex-1 flex-col gap-10 px-3 pt-6 sm:pb-22 lg:flex-row lg:pt-14 xl:pb-20">
+    <main className="3xl:pb-14 mx-auto flex w-full max-w-[1300px] flex-1 flex-col gap-10 px-3 pt-6 sm:pb-22 lg:flex-row lg:pt-14 xl:pb-20 2xl:px-0">
       {!isMobile && <Sidebar />}
       {children}
     </main>
