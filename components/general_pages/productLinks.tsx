a4f649134dcf156fe815e55f250d9eb2e431ff6a
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-23 17:41:16 +0400

    ProductLinks: dynamic grid cols

--- components/general_pages/productLinks.tsx
+++ components/general_pages/productLinks.tsx
@@ -13,7 +13,7 @@ export default function ProductLinks({
   return (
     <ul
       className={cn(
-        "grid grid-cols-3 gap-x-5 gap-y-6 sm:grid-cols-4 md:grid-cols-5 lg:gap-x-3 xl:gap-x-5",
+        "grid grid-cols-[repeat(auto-fill,minmax(104px,max-content))] gap-x-5 gap-y-6 lg:gap-x-3 xl:gap-x-5",
         className,
       )}
     >
