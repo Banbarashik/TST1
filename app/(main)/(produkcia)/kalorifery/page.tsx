0e8d22129891310f8b66dca28c361912fd7cafa9
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-26 12:55:21 +0400

    kalorifery: create kaloriferyCards array

--- app/(main)/(produkcia)/kalorifery/page.tsx
+++ app/(main)/(produkcia)/kalorifery/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Калориферы. Производство",
   description:
@@ -5,6 +7,65 @@ export const metadata: Metadata = {
   keywords:
     "калорифер,производство калориферов,промышленный калорифер,производство промышленных калориферов,калориферы производитель,калорифер цена,калорифер купить,калорифер водяной,калорифер паровой,каталог калориферов",
 };
+
+const kaloriferyCards = [
+  [
+    {
+      name: "Калорифер",
+      url: "",
+      img: "/img/produkcia/kalorifery/",
+    },
+    {
+      name: "Калорифер водяной и паровой",
+      url: "",
+      img: "/img/produkcia/kalorifery/",
+    },
+  ],
+  [
+    {
+      name: "Калориферы КПВС КПВУ",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_kpvs_kpvu.png",
+    },
+    {
+      name: "Калориферы КППС КППУ",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_kpps_kppu.png",
+    },
+    {
+      name: "Калориферы КСк",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_ksk.png",
+    },
+    {
+      name: "Калориферы КПСк",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_kpsk.png",
+    },
+  ],
+  [
+    {
+      name: "Калориферы ТВВ",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_tvv.png",
+    },
+    {
+      name: "Калориферы КП",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_kp.png",
+    },
+    {
+      name: "Калориферы КФБ М",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_kfb-m.png",
+    },
+    {
+      name: "Калориферы КФБ П",
+      url: "",
+      img: "/img/produkcia/kalorifery/kalorifer_kfb-p.png",
+    },
+  ],
+];
 
 export default function KaloriferyPage() {
   return <></>;
