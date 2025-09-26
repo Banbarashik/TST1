3e8b17db6b6591f191c63b4c093323b862086742
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-26 14:13:24 +0400

    CategoryCards: rework

--- components/categoryCards.tsx
+++ components/categoryCards.tsx
@@ -1,26 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CategoryCards({ categories }) {
+import { cn } from "@/lib/utils";
+
+export default function CategoryCards({
+  categories,
+  containerGap = "",
+  textSize = "",
+}) {
   return (
-    <ul className="mt-6 mb-6 flex flex-wrap gap-2 sm:flex-nowrap sm:gap-2 md:gap-4 lg:gap-10 xl:gap-12">
+    <ul
+      className={cn(
+        "flex flex-col gap-2 sm:flex-row sm:gap-2 md:gap-4 lg:gap-10 xl:gap-12",
+        containerGap,
+      )}
+    >
       {categories.map((cat) => (
         <CategoryCard
           key={cat.name}
           category={cat}
-          className={`${categories.length === 2 ? "w-full" : "w-[48%]"} sm:w-full`}
+          siblingsAmount={categories.length}
+          textSize={textSize}
         />
       ))}
     </ul>
   );
 }
 
-function CategoryCard({ category, className }) {
+function CategoryCard({ category, siblingsAmount, textSize = "" }) {
   return (
-    <li className={className}>
+    <li className={`sm:w-full ${siblingsAmount === 2 ? "w-full" : "w-[48%]"}`}>
       <Link
         href={category.url}
-        className="hover:text-primary bg-card text-card-foreground flex h-full flex-col items-center gap-4 rounded-xl border px-2 pt-5 pb-5 text-center shadow-sm sm:text-[13px] md:px-4 md:text-center md:text-[13px] lg:h-auto lg:px-4 lg:text-sm xl:px-10 2xl:text-base"
+        className={cn(
+          "hover:text-primary bg-card text-card-foreground flex h-full flex-col items-center justify-between gap-4 rounded-xl border px-2 pt-5 pb-5 text-center shadow-sm sm:text-[13px] md:px-4 md:text-center md:text-[13px] lg:px-4 lg:text-sm xl:px-10 2xl:text-base",
+          textSize,
+        )}
       >
         <Image
           src={category.img}
