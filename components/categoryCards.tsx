5bfa2cbefeb5a27fa171185915a3ef81edd8ec04
Banbarashik <odinokyskitalec@yahoo.com>
2025-09-27 13:22:31 +0400

    CategoryCards: accept 'cardClassName' instead of 'textSize'

--- components/categoryCards.tsx
+++ components/categoryCards.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 export default function CategoryCards({
   categories,
   containerGap = "",
-  textSize = "",
+  cardClassName = "",
 }) {
   return (
     <ul
@@ -20,21 +20,21 @@ export default function CategoryCards({
           key={cat.name}
           category={cat}
           siblingsAmount={categories.length}
-          textSize={textSize}
+          className={cardClassName}
         />
       ))}
     </ul>
   );
 }
 
-function CategoryCard({ category, siblingsAmount, textSize = "" }) {
+function CategoryCard({ category, siblingsAmount, className }) {
   return (
     <li className={`sm:w-full ${siblingsAmount === 2 ? "w-full" : "w-[48%]"}`}>
       <Link
         href={category.url}
         className={cn(
           "hover:text-primary bg-card text-card-foreground flex h-full flex-col items-center justify-between gap-4 rounded-xl border px-2 pt-5 pb-5 text-center shadow-sm sm:text-[13px] md:px-4 md:text-center md:text-[13px] lg:px-4 lg:text-sm xl:px-10 2xl:text-base",
-          textSize,
+          className,
         )}
       >
         <Image
