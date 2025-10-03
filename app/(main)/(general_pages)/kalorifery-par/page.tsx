aba8247075008e35f65599a52fee7c7a60b8dc9c
Banbarashik <odinokyskitalec@yahoo.com>
2025-10-03 17:06:53 +0400

    kalorifery-par: use LinkButtonsBlock

--- app/(main)/(general_pages)/kalorifery-par/page.tsx
+++ app/(main)/(general_pages)/kalorifery-par/page.tsx
@@ -8,7 +8,7 @@ import { sortProducts } from "@/lib/utils";
 import Heading from "@/components/general_pages/heading";
 import ProductParagraph from "@/components/catalog/productParagraph";
 import ProductLinks from "@/components/general_pages/productLinks";
-import TableAndCatalogLinks from "@/components/catalog/tableAndCatalogLinks";
+import LinkButtonsBlock from "@/components/linkButtonsBlock";
 
 export const metadata: Metadata = {
   title: "Калориферы приточные паровые",
@@ -17,6 +17,19 @@ export const metadata: Metadata = {
   keywords:
     "калорифер приточный паровой,калорифер паровой для приточной вентиляции,калорифер паровой для приточной установки,расчет парового приточного калорифера,подбор приточного калорифера парового,паровые приточные калориферы цена,приточный паровой калорифер купить,паровые приточные калориферы характеристики,приточные паровые калориферы размеры,паровая приточная установка",
 };
+
+const linkButtons = [
+  {
+    name: "Каталог паровых калориферов",
+    url: "/documents/Kalorifer_KPPS_KPPU_katalog_2025.pdf",
+    openNewTab: true,
+  },
+  {
+    name: "Прайс-лист паровых калориферов",
+    url: "/documents/Price_list_zao_tst_2025.pdf",
+    openNewTab: true,
+  },
+];
 
 export default function KaloriferyParPage() {
   const kpps = productData
@@ -164,13 +177,7 @@ export default function KaloriferyParPage() {
         />
       </section>
 
-      <TableAndCatalogLinks
-        tableLinkOpenNewTab
-        tableURL="/documents/Kalorifer_KPPS_KPPU_katalog_2025.pdf"
-        tableLinkText="Скачать каталог приточных паровых калориферов"
-        catalogURL="/documents/Price_list_zao_tst_2025.pdf"
-        catalogLinkText="Скачать прайс-лист приточных паровых калориферов"
-      />
+      <LinkButtonsBlock buttons={linkButtons} />
     </>
   );
 }
