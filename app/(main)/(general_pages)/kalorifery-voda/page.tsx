3df357a4bb7740f18e0abd939c7080a0ef7c82b2
Banbarashik <odinokyskitalec@yahoo.com>
2025-10-14 19:32:57 +0400

    kalorifery-voda: change anchor2 placement

--- app/(main)/(general_pages)/kalorifery-voda/page.tsx
+++ app/(main)/(general_pages)/kalorifery-voda/page.tsx
@@ -90,6 +90,7 @@ export default function KaloriferyVodaPage() {
 
       <section className="space-y-6">
         <section>
+          <span id="anchor2" className="invisible relative -top-13" />
           <Heading lvl={2} text="Онлайн-расчет мощности водяного калорифера" />
           <ProductParagraph className="mb-3">
             Расход тепла водяным калорифером на подогрев приточного воздуха. В
@@ -155,7 +156,6 @@ export default function KaloriferyVodaPage() {
       </section>
 
       <section>
-        <span id="anchor2" className="invisible relative -top-13" />
         <Heading lvl={2} text="Расчет и подбор приточных водяных калориферов" />
         <ProductParagraph>
           Выбрав номер калорифера с приближенным для выполнения вашей задачи
