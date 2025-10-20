c3c08faf76b1a73fb199364057613e635c3fb9df
Banbarashik <odinokyskitalec@yahoo.com>
2025-10-20 18:38:56 +0400

    restyle nav menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ProductSelectionProvider } from "@/context/ProductSelectionContext";
 
 import "@/app/globals.css";
 
-import { Phone, Smartphone, Mail } from "lucide-react";
+import { PhoneCall, Mail } from "lucide-react";
 
 import YandexMetrikaContainer from "@/components/YandexMetrikaContainer";
 
@@ -29,45 +29,35 @@ export default function RootLayout({
           </Suspense>
         </head>
         <body className="font-arial flex min-h-screen flex-col antialiased">
-          <nav className="4xl:px-20 flex h-22 items-center bg-[#E0E0E0] pr-3 pl-6 sm:pr-6">
+          <nav className="4xl:px-20 flex h-25 items-center border-t-[1.5px] border-b-[1.5px] border-[#A5A5A5] bg-[#e0e0e0] pr-3 pl-6 sm:pr-6">
             <Logo place="header" />
-            <div className="4xl:ml-12 3xl:ml-8 ml-6 flex flex-col items-start gap-y-1.5 text-lg sm:flex-row md:text-base lg:flex-col xl:flex-row">
-              <a
-                href="mailto:zao_tst@mail.ru"
-                className="text-primary-darker hidden items-center gap-2 lg:hidden"
-              >
-                <Mail className="size-4" />
-                zao_tst@mail.ru
-              </a>
-              <ul className="text-primary-darker mr-5 space-y-2">
-                <li className="hidden items-center gap-2.5 sm:flex lg:hidden xl:flex">
-                  <Phone className="size-4 lg:size-5" />
-                  +7 (3846) 68-23-24
-                </li>
-                <li>
-                  <a
-                    href="tel:89617378314"
-                    className="flex items-center gap-2.5 hover:font-semibold hover:text-[#604890]"
-                  >
-                    <Smartphone className="size-4 lg:size-5" />
-                    8-961-737-83-14
-                  </a>
-                </li>
-              </ul>
-              <a
-                href="mailto:zao_tst@mail.ru"
-                className="text-primary-darker flex items-center gap-2 lg:order-first lg:flex xl:order-none"
-              >
-                <Mail className="size-4 xl:size-5" />
-                zao_tst@mail.ru
-              </a>
-            </div>
+            <ul className="4xl:ml-12 3xl:ml-8 text-primary-darker ml-6 flex flex-col items-start gap-y-2 text-lg">
+              <li className="hover:font-semibold">
+                <a href="tel:89617378314" className="flex items-center gap-2.5">
+                  <span className="border-primary-darker rounded-full border p-1.25">
+                    <PhoneCall className="size-5" />
+                  </span>
+                  <span>+7 (961) 737-83-14</span>
+                </a>
+              </li>
+              <li className="hover:text-shadow-[0_0_1px_currentColor]">
+                <a
+                  href="mailto:zao_tst@mail.ru"
+                  className="flex items-center gap-2.5"
+                >
+                  <span className="border-primary-darker rounded-full border p-1.25">
+                    <Mail className="size-5" />
+                  </span>
+                  <span>zao_tst@mail.ru</span>
+                </a>
+              </li>
+            </ul>
             <div className="ml-auto hidden h-full items-center gap-3 lg:flex xl:gap-6">
               <NavigationMenu />
               <ContactFormTrigger
                 triggerBtnVariant="default"
                 triggerBtnSize="lg"
-                triggerBtnClassName="h-12 cursor-pointer text-base font-bold xl:text-xl"
+                triggerBtnClassName="h-12 cursor-pointer hover:bg-primary-dark text-base font-bold xl:text-xl border border-[#A5A5A5]"
                 amountClassName="bg-accent absolute text-black right-0 bottom-0 inline-flex size-6 translate-x-1/2 translate-y-1/2 items-center justify-center rounded-full text-sm font-semibold"
               />
             </div>
