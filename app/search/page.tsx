834c28b09924b35d70948ebfdab2ec8989e5a5e7
Banbarashik <odinokyskitalec@yahoo.com>
2025-10-29 18:48:58 +0400

    search page: always show y scroll

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,7 +8,8 @@ export default async function Page({
   const { q } = await searchParams;
   const query = typeof q === "string" ? q : "";
   return (
-    <div className="flex grow flex-col pb-18">
+    <div className="relative flex grow flex-col pb-18">
+      <div className="3xl:-bottom-[160px] bg-secondary absolute hidden h-[0.1px] w-full lg:-bottom-[196.5px] lg:block"></div>
       {/* Allow SearchResults to grow */}
       <SearchResults initialQuery={query} />
     </div>
