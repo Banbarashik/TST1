b2284332a9d096f213c1280b69b7800dda2f6db0
Banbarashik <odinokyskitalec@yahoo.com>
2025-11-02 19:18:01 +0400

    STDPage: checks for 1st tables

--- components/catalog/STDPage.tsx
+++ components/catalog/STDPage.tsx
@@ -275,34 +275,36 @@ export default function STDPage({ product }) {
             </tbody>
           </table>
         </div>
-        <div className="flex flex-col gap-4 sm:flex-row md:gap-6 lg:gap-4 xl:gap-10">
-          <div
-            className={`relative w-full`}
-            style={{
-              aspectRatio: `${product.frontView.width}/${product.frontView.height}`,
-            }}
-          >
-            <Image
-              src={product.frontView.url}
-              alt={`${capitalizeFirst(heatCarrierAdj.nom)} воздушный агрегат ${product.shortName}`}
-              title={`Агрегат ${product.shortName} ${heatCarrierAdj.nom}`}
-              fill
-            />
+        {product.frontView && product.parts && (
+          <div className="flex flex-col gap-4 sm:flex-row md:gap-6 lg:gap-4 xl:gap-10">
+            <div
+              className={`relative w-full`}
+              style={{
+                aspectRatio: `${product.frontView.width}/${product.frontView.height}`,
+              }}
+            >
+              <Image
+                src={product.frontView.url}
+                alt={`${capitalizeFirst(heatCarrierAdj.nom)} воздушный агрегат ${product.shortName}`}
+                title={`Агрегат ${product.shortName} ${heatCarrierAdj.nom}`}
+                fill
+              />
+            </div>
+            <div
+              className={`relative w-full`}
+              style={{
+                aspectRatio: `${product.parts.width}/${product.parts.height}`,
+              }}
+            >
+              <Image
+                src={product.parts.url}
+                title={`${capitalizeFirst(heatCarrierAdj.nom)} отопительный агрегат СТД-300`}
+                alt={`Воздушно-отопительный агрегат ${product.shortName} ${heatCarrierAdj.nom}`}
+                fill
+              />
+            </div>
           </div>
-          <div
-            className={`relative w-full`}
-            style={{
-              aspectRatio: `${product.parts.width}/${product.parts.height}`,
-            }}
-          >
-            <Image
-              src={product.parts.url}
-              title={`${capitalizeFirst(heatCarrierAdj.nom)} отопительный агрегат СТД-300`}
-              alt={`Воздушно-отопительный агрегат ${product.shortName} ${heatCarrierAdj.gen}`}
-              fill
-            />
-          </div>
-        </div>
+        )}
       </section>
 
       <ProductSubheader
